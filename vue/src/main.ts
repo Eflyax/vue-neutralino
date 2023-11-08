b0128966e9fd8088b8188ba577c188a369626e6f
Jakub Záruba <jakuba.zaruba@orgis.cz>
2023-11-08 14:30:02 +0100

    Add VSCode tasks

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,8 +1,6 @@
 import {createApp} from 'vue';
-import * as mdijs from '@mdi/js';
 import App from './App.vue';
 import i18n from './localization/i18n';
-import mdiVue from 'mdi-vue/v3';
 import mitt from 'mitt';
 import router from './router';
 
@@ -15,7 +13,4 @@ app.config.globalProperties.standalone = false;
 
 app.use(router)
 	.use(i18n)
-	.use(mdiVue, {
-		icons: mdijs
-	})
 	.mount('#app');
