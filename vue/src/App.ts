fc1f8cfa64d23828d9c3d3b533855d55e48b6e9b
Jakub Záruba <jakuba.zaruba@orgis.cz>
2023-11-10 12:26:19 +0100

    WIP

--- vue/src/App.ts
+++ vue/src/App.ts
@@ -1,5 +1,6 @@
-import Default from '@/src/layouts/Default.vue';
+import {filesystem} from '@neutralinojs/lib';
 import authInfo from '../../.tmp/auth_info.json';
+import Default from '@/src/layouts/Default.vue';
 
 export default {
 	components: {
@@ -9,17 +10,41 @@ export default {
 		return {
 		};
 	},
-	async mounted() {
+	mounted() {
+		console.log('Mounted');
+
 		if (import.meta.env.DEV) {
 			const
 				{accessToken, port} = authInfo;
 
 			window.NL_PORT = port;
 			window.NL_TOKEN = accessToken;
+			window.NL_ARGS = [];
 		}
 
-		const list = await window.Neutralino.os.execCommand('ls');
+		window.Neutralino.init();
 
-		console.log({list});
+		// console.log({'windNeu': window.Neutralino});
+		// console.log({'windNeu': window.Neutralino.os.getEnvs()});
+
+		// window.Neutralino.os.getEnvs().then(foo => {
+		// 	console.log({foo});
+		// });
+		// window.Neutralino.os.execCommand('./vue/dist/server')
+
+		// setTimeout(() => {
+		// 	console.log({OS: window});
+
+		// 	console.log('now');
+		// 	window.Neutralino.os.execCommand('ls -la')
+		// 		.then((foo) => {
+		// 			console.log(foo);
+		// 		});
+		// }, 2_000);
+
+		window.Neutralino.os.execCommand('ls -la')
+			.then(foo => {
+				console.log(foo.stdOut.toString());
+			});
 	}
 };
